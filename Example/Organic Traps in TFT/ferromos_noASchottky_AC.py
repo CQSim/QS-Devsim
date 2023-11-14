8ccc73576342fcf70a9aa73adfcadcb094276c6a
CQSim <45313107+CQSim@users.noreply.github.com>
2023-11-14 13:48:26 +0800

    Update ferromos_noASchottky_AC.py

--- Example/Organic Traps in TFT/ferromos_noASchottky_AC.py
+++ Example/Organic Traps in TFT/ferromos_noASchottky_AC.py
@@ -1,8 +1,7 @@
 import devsim
 import sys, numpy, os,  tarfile
 
-sys.path.append('/home/ccc/devsim/QS_Micro_packages/')
-sys.path.append('./')
+sys.path.append('../../')
 from QSsimple_physics import *
 from QSPlotSweep import *
 from QSFerro import *
